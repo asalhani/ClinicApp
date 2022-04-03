2af6e6fa6d87d8126e9ad8944aefba416107f3c2
Adib Ahmad Salhani <asalhani@elm.sa>
2022-04-03 09:28:13 +0300

    Apply html template

--- Client2/src/app/app-routing.module.ts
+++ Client2/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { InjuryInfoComponent } from './injury-info/injury-info.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'home', component: HomeComponent },
+   { path: 'injuries', component: InjuryInfoComponent },
+   { path: '**', component:  HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ Client2/src/app/injury-info/injury-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-injury-info',
+  templateUrl: './injury-info.component.html',
+  styleUrls: ['./injury-info.component.css']
+})
+export class InjuryInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
